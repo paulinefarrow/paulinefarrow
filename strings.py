a4c1779ebd0a381e9e7a61545ced7045ffd591e0
paulinefarrow <154377220+paulinefarrow@users.noreply.github.com>
2023-12-31 22:02:14 +0000

    Delete strings.py

--- strings.py
+++
@@ -1,7 +0,0 @@
-# Declare a variable hero and assign it to a value
-
-hero = "$$$Superman$$$"
-
-# Use the string manipulation method strip() to print hero so that the output is superman
-
-print(hero.strip('$'))
