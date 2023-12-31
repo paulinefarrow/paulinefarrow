bda2fb001c25076829f663af39a1804ba23212fc
paulinefarrow <154377220+paulinefarrow@users.noreply.github.com>
2023-12-31 21:59:43 +0000

    Delete logic.py

--- logic.py
+++
@@ -1,9 +0,0 @@
-# print a symmetrical shape out of @ symbols using a for loop, with a logical error
-
-for i in range(1, 19):
-    if i <= 10:
-        print((10-i)*" " + (2*i*"@") + (10-i)*" " )
-    if i > 10:
-        print((i-10)*" " + (2*(19-i)*"@") + (i-10)*" " )
-
-
